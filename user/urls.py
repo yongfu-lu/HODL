ef1fb5ed208abdd7008b36437df22c47490f1109
yongfu-lu <yongfu2015@gmail.com>
2023-01-17 18:35:48 -0500

    create pages for user dashboard

--- user/urls.py
+++ user/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('dashboard/', views.dashboard, name='dashboard'),
-    path('logout', views.logout, name='logout')
+    path('logout', views.logout, name='logout'),
+    path('algorithms/', views.algorithms, name='algorithms'),
+    path('data-analysis/', views.dataAnalysis, name='dataAnalysis'),
+    path('recommendations/', views.recommendations, name='recommendations'),
+    path('user-api/', views.userAPI, name='userAPI'),
 ]
