5750aea38cc389670d3a21820e1248bdf4ee3629
Stefan Tan <33496362+stefantan18@users.noreply.github.com>
2023-02-16 20:01:04 -0500

    added ATR strategy

--- Backtester/strategy.py
+++ Backtester/strategy.py
@@ -45,5 +45,41 @@ class Strategy:
             else:
                 self.portfolio = self.investment
 
+    def execute_atr(self, start, end, symbol, short, long):
+        atr = ind.Indicator(self.client, symbol)
+        atr_short = atr.ATR(start, end, short)
+        atr_short.rename(columns = {'ATR':'short'}, inplace = True)
+        atr_long = atr.ATR(start, end, long)
+        atr_long.rename(columns = {'ATR':'long'}, inplace = True)
+        position = 'cash'
+
+        request_params = StockBarsRequest(symbol_or_symbols=[symbol],
+                                          timeframe = TimeFrame.Day,
+                                          start = start,
+                                          end = end) # type: ignore
+
+        bars = self.client.get_stock_bars(request_params)
+        data = bars.df['close']
+
+        df = pd.concat([atr_short, atr_long, data], axis=1)
+
+        prev_close = 0
+
+        for index, row in df.iterrows():
+            if (prev_close != 0) and (row['long'] + row['close'] > prev_close) and (position == 'cash'):
+                shares = self.investment / row['close']
+                self.investment = 0
+                position = 'long'
+            elif (prev_close != 0) and (row['long'] + row['close'] < prev_close) and (position == 'long'):
+                self.investment = shares * row['close'] - self.commission # type: ignore
+                shares = 0
+                position = 'cash'
+            if position == 'long':
+                self.portfolio = shares * row['close'] # type: ignore
+            else:
+                self.portfolio = self.investment
+                
+            prev_close = row['close']    
+
     def getVal(self):
         return self.portfolio
