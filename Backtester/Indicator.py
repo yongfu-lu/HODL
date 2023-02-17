c125d59a5eb2028f81158d660d818aa65366d374
Ahmed Imran <aimran1239@gmail.com>
2023-02-16 20:18:17 -0500

    Add Fibonacci and MACD indicator and strategy

--- Backtester/Indicator.py
+++ Backtester/Indicator.py
@@ -61,3 +61,53 @@ class Indicator:
         df = RSI.to_frame()
 
         return df
+
+    def FibLevels(self, s , e):
+        request_params = StockBarsRequest(symbol_or_symbols=[self.symbol],
+                                          timeframe = TimeFrame.Day,
+                                          start = s,
+                                          end = e)
+        bars = self.api.get_stock_bars(request_params)
+        swing_max = bars.df['close'].max()
+        swing_min = bars.df['close'].min()
+
+        diff = swing_max - swing_min
+        level1 = swing_max - diff*0.236
+        level2 = swing_max - diff*0.382
+        level3 = swing_max - diff*0.5
+        level4 = swing_max - diff*0.618
+        return [swing_max, level1, level2, level3, level4, swing_min]
+    
+    def MACD(self, s , e, short, long):
+        request_params = StockBarsRequest(symbol_or_symbols=[self.symbol],
+                                          timeframe = TimeFrame.Day,
+                                          start = s,
+                                          end = e)
+        bars = self.api.get_stock_bars(request_params)
+        short_EMA = bars.df['close'].ewm(span=short, adjust=False).mean()
+        long_EMA = bars.df['close'].ewm(span=long, adjust=False).mean()
+        MACD = short_EMA - long_EMA
+        signal = MACD.ewm(span=9, adjust=False).mean()
+        MACD = MACD.rename("MACD")
+        signal = signal.rename("signal")
+        df = MACD.to_frame().join(signal.to_frame())
+        return df
+
+
+
+
+trading_client = StockHistoricalDataClient('PKV2FZHX6E4RMGFON60X',
+                                           'GMKXVZ3W4MqenB6SbcSKM8h9WnvYBZn0qdZ86E6n')
+
+ind = Indicator(trading_client, "AAPL")
+
+x = datetime(2020, 5, 7)
+y = datetime(2021, 5, 7)
+
+#s1 = (ind.MACD(x, y, 12, 9)).to_string()
+#s2 = (ind2.bollinger_bands(x, y, 20, 2)).to_string()
+#s3 = (ind3.RSI(x, y, 20)).to_string()
+
+#print(s1)
+#print(s2)
+#print(s3)
--- Backtester/strategy.py
+++ Backtester/strategy.py
@@ -37,7 +37,7 @@ class Strategy:
                 self.investment = 0
                 position = 'long'
             elif (row['short'] < row['long']) and (position == 'long'):
-                self.investment = shares * row['close'] - commission
+                self.investment = shares * row['close'] - self.commission
                 shares = 0
                 position = 'cash'
             if position == 'long':
@@ -45,5 +45,70 @@ class Strategy:
             else:
                 self.portfolio = self.investment
 
+    def execute_fib(self, start, end, symbol, short, long):
+        fib = ind.Indicator(self.client, symbol)
+        fib_signals = fib.MACD(start, end, short, long)
+        fib_levels = fib.FibLevels(start, end)
+
+        def get_level(price):
+            if price >= fib_levels[1]:
+                return fib_levels[0], fib_levels[1]
+            elif price >= fib_levels[2]:
+                return fib_levels[1], fib_levels[2]
+            elif price >= fib_levels[3]:
+                return fib_levels[2], fib_levels[3]
+            elif price >= fib_levels[4]:
+                return fib_levels[3], fib_levels[4]
+            else:
+                return fib_levels[4], fib_levels[5]
+
+        request_params = StockBarsRequest(symbol_or_symbols=[symbol],
+                                          timeframe = TimeFrame.Day,
+                                          start = start,
+                                          end = end)
+
+        bars = self.client.get_stock_bars(request_params)
+        data = bars.df['close']
+        df2 = data.to_frame()
+
+        df = pd.concat([fib_signals, data], axis=1)
+
+        position = 'cash'
+        hi_level, lo_level = None, None
+        buy_price = 0
+
+        for index, row in df.iterrows():
+            price = row['close']
+            if (hi_level == None or lo_level == None):
+                hi_level, lo_level = get_level(price)
+                continue
+            elif(price >= hi_level) or (price <= lo_level): ## Enter new level
+                if (row['signal'] > row['MACD']) and (position == 'cash'): # buy
+                    shares = self.investment / row['close']
+                    self.investment = 0
+                    position = 'long'
+                    buy_price = price
+                elif (row['signal'] < row['MACD']) and (position == 'long') and price > buy_price: # sell
+                    self.investment = shares * row['close'] - self.commission
+                    shares = 0
+                    position = 'cash'
+                    buy_price = 0
+            if position == 'long':
+                self.portfolio = shares * row['close']
+            else:
+                self.portfolio = self.investment
+            hi_level, lo_level = get_level(price)
+
     def getVal(self):
         return self.portfolio
+
+trading_client = StockHistoricalDataClient('PKV2FZHX6E4RMGFON60X',
+                                           'GMKXVZ3W4MqenB6SbcSKM8h9WnvYBZn0qdZ86E6n')
+
+x = datetime(2020, 5, 17)
+y = datetime(2022, 5, 17)
+
+moving_average = Strategy(trading_client, 10000, 10000, 5)
+print(moving_average.getVal())
+moving_average.execute_fib(x, y, "AAPL", 12, 9)
+print(moving_average.getVal())
