fb883d56436ab8a96c855782cf61af36bfc2848f
yongfu-lu <yongfu2015@gmail.com>
2023-02-27 14:36:34 -0500

    update activated-algo model, add de-activate button for algo

--- user/models.py
+++ user/models.py
@@ -9,16 +9,18 @@ class CustomUser(AbstractUser):
     secret_key = models.CharField(max_length=255, blank=True)
 
 class ActivatedAlgorithm(models.Model):
+    id = models.AutoField(primary_key=True)
     algorithm = models.CharField(max_length=100)
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     stock_name = models.CharField(max_length=100)
     investment_amount = models.DecimalField(max_digits=10, decimal_places=2)
-    short_moving_avg = models.DecimalField(max_digits=10, decimal_places=2,  blank=True, null=True)
-    long_moving_avg = models.DecimalField(max_digits=10, decimal_places=2,  blank=True, null=True)
-    days_of_moving_avg = models.PositiveIntegerField( blank=True, null=True)
-    over_percentage_threshold = models.DecimalField(max_digits=5, decimal_places=2,  blank=True, null=True)
-    under_percentage_threshold = models.DecimalField(max_digits=5, decimal_places=2,  blank=True, null=True)
-    standard_deviation = models.DecimalField(max_digits=10, decimal_places=2,  blank=True, null=True)
+    shares = models.PositiveIntegerField(default=0)
+    short_moving_avg = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    long_moving_avg = models.DecimalField(max_digits=10, decimal_places=2,  default=0)
+    days_of_moving_avg = models.PositiveIntegerField(default=0)
+    over_percentage_threshold = models.DecimalField(max_digits=5, decimal_places=2, default=0)
+    under_percentage_threshold = models.DecimalField(max_digits=5, decimal_places=2, default=0)
+    standard_deviation = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
     def __str__(self):
         return f"{self.user}  {self.algorithm}  {self.stock_name}"
