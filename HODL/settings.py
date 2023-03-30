5f9bf7113006b4ead47e26413ca6084f27790a0f
araheem5134 <104283115+araheem5134@users.noreply.github.com>
2023-03-30 14:31:13 -0400

    Add files via upload

--- HODL/settings.py
+++ HODL/settings.py
@@ -91,10 +91,10 @@ DATABASES = {
         'HOST': env('AWS_DATABASE_HOST'),
         'PORT': '5432',
     }
-     #"default": {
+    #"default": {
          #"ENGINE": "django.db.backends.sqlite3",
          #"NAME": BASE_DIR / "db.sqlite3",
-     #}
+    #}
 }
 
 
